3a9cb7fbb703801b8e4336ff5a2cbf5ece38fd95
Pedro Vergne <peu.lima204@gmail.com>
2026-03-04 18:48:31 -0300

    Add first Java RDD exercises

---
+++ ex01/src/Perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    public static void main(){
+        String nome = "Pedro";
+        System.out.println("Olá, "+nome);
+        System.out.println("Tudo bem?");
+
+    }
+}
---
+++ ex01/src/Subtração.java
@@ -0,0 +1,8 @@
+public class Subtração {
+    public static void main() {
+        int num1 = 5;
+        int num2 = 10;
+        int resultado = num2 - num1;
+        System.out.println("A subtração deu: " + resultado);
+    }
+}
